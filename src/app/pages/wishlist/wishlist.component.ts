25a9e4b7bf94a1b47cd0576ac06492145f127322
Sunntle <taikute012532@gmail.com>
2023-06-11 10:52:48 +0700

    Add to cart in wishlist

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -7,15 +7,16 @@ import { Product } from 'src/app/interface/product';
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['./wishlist.component.scss']
+  styleUrls: ['./wishlist.component.scss'],
 })
 export class WishlistComponent implements OnInit {
   wishList: Product[] = [];
+  quantity: number = 0;
   constructor(
     private messageService: MessageService,
     private cartService: CartService,
     private wish: WishListService
-  ) { }
+  ) {}
   ngOnInit(): void {
     this.wish.getWishList().subscribe((data: any) => {
       this.wishList = data;
@@ -23,5 +24,10 @@ export class WishlistComponent implements OnInit {
   }
   delWish(id: string) {
     this.wish.delWish(id);
+  }
+  addToCart(product: Product) {
+    product.quantity = ++this.quantity;
+    product.sizeSelected = 'M';
+    this.cartService.addToCart(product);
   }
 }
