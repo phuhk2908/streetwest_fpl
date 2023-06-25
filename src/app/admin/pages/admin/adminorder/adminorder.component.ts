ff4947f04c49c7e5a75e35e3d2ebc8e04319953f
legiahuy060903 <huylg147258369@gmail.com>
2023-06-25 10:49:29 +0700

    huy

--- src/app/admin/pages/admin/adminorder/adminorder.component.ts
+++ src/app/admin/pages/admin/adminorder/adminorder.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormBuilder } from '@angular/forms';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { Table } from 'primeng/table';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { OrderService } from 'src/app/core/services/order.service';
 import { ProductService } from 'src/app/core/services/product.services';
 
@@ -11,14 +13,19 @@ import { ProductService } from 'src/app/core/services/product.services';
   templateUrl: './adminorder.component.html',
   styleUrls: ['./adminorder.component.scss']
 })
-export class AdminorderComponent implements OnInit {
+export class AdminorderComponent implements OnInit, OnDestroy {
   orders: any[] = [];
   ordersDetail: any;
   sidebar: boolean = false;
+  sidebarEdit: boolean = false;
+
+  user: any;
   constructor(private pd: ProductService, private messageService: MessageService,
     private _fb: FormBuilder, private storage: AngularFireStorage,
-    private confirmationService: ConfirmationService, private od: OrderService) { }
+    private confirmationService: ConfirmationService,
+    private od: OrderService, private AuthService: AuthService) { }
 
+  private subscription: Subscription = new Subscription();
   ngOnInit() {
     this.getData();
   }
@@ -27,13 +34,27 @@ export class AdminorderComponent implements OnInit {
       this.orders = res;
     });
   }
-  async showDetail(id: string) {
+  showDetail(order: any) {
     this.sidebar = true;
-    console.log(id);
-    this.od.getIdDetailOrder(id).subscribe(res => {
-      this.ordersDetail = res;
-      console.log(this.ordersDetail.cart);
+    this.od.getIdDetailOrder(order.idOrder).subscribe(res => {
+      this.ordersDetail = { ...order, ...res };
+
+      this.AuthService.getRole(order.userID).then(user => {
+        this.user = user;
+      })
     })
+  }
+  handelEdit(order: any) {
+    this.od.getIdDetailOrder(order.idOrder).subscribe(res => {
+      this.ordersDetail = { ...order, ...res };
+      this.AuthService.getRole(order.userID).then(user => {
+        this.user = user;
+      })
+    })
+  }
+
+  onHide() {
+    this.ordersDetail = []
   }
   clear(table: Table) {
     table.clear();
@@ -49,5 +70,11 @@ export class AdminorderComponent implements OnInit {
         })
       }
     });
+  }
+  ngOnDestroy() {
+    if (this.subscription) {
+      console.log('unsubscribe');
+      this.subscription.unsubscribe();
+    }
   }
 }
