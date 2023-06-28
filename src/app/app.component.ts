1f77af06d81545278cd92c8635cbaa842b5067a7
Sunntle <taikute012532@gmail.com>
2023-06-28 21:24:27 +0700

    add messenger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, inject } from '@angular/core';
 
 import { Analytics, logEvent } from '@angular/fire/analytics';
 import { Router, NavigationEnd } from '@angular/router';
+declare global {
+  interface Window {
+    FB: any;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -21,6 +26,7 @@ export class AppComponent implements OnInit {
         this.updateHeaderVisibility();
       }
     });
+    this.runMess();
   }
   private updateHeaderVisibility() {
     const currentRoute = this.router.url;
@@ -43,5 +49,26 @@ export class AppComponent implements OnInit {
     } else {
       this.showHeader = true;
     }
+  }
+  runMess() {
+    let chatbox = document.getElementById('fb-customer-chat')!;
+    chatbox.setAttribute('page_id', '112962281510919');
+    chatbox.setAttribute('attribution', 'biz_inbox');
+    (window as any).fbAsyncInit = function () {
+      this.FB.init({
+        xfbml: true,
+        version: 'v17.0',
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s) as HTMLScriptElement;
+      js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode?.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
   }
 }
